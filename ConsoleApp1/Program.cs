1553ba62fd96b77089d6ce9dd567da98e2be264d
l4nst <l4nst@LAPTOP-HE9JLDVE>
2022-09-24 14:08:41 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            //სავარჯიშო 1
+             /* ეტაპები
+              * 1 - კომპილაცია
+              * 2 - CIL  Comman Intermediate Language
+              * 3 - CLR Comman Language Runtime
+              * 4 - Machine COde
+              */
+            
+            // სავარჯიშო 2 
+
+            //Console.WriteLine("first number");
+            //int firstNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("second number");
+            //int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("the sum of " + firstNum + " and " + secondNum + " equals " + (firstNum + secondNum));
+            //Console.WriteLine("the difference of " + firstNum + " and " + secondNum + " equals " + (firstNum - secondNum));
+            //Console.WriteLine("the Multiplication of " + firstNum + " and " + secondNum + " equals " + (firstNum * secondNum));
+
+
+
+            // სავარჯიშო 3
+
+            //Console.WriteLine("first number: ");
+            //int firstNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("second number: ");
+            //int secondNum = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("the perimeter of the rectangle is " + 2 * (firstNum + secondNum));
+            //Console.WriteLine("the area of the rectangle is " + (firstNum * secondNum));
+
+
+
+            // სავარჯიშო 4
+
+            //Console.WriteLine("first number: ");
+            //int firstNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("second number: ");
+            //int secondNum = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("third number: ");
+            //int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+            //int[] list = { firstNum, secondNum, thirdNum };
+            //Array.Sort(list);
+
+            //foreach (int value in list)
+            //{
+            //    Console.Write(value + " ");
+            //}
+
+
+
+            // სავარჯიშო 5
+
+            //Console.WriteLine("Enter the 3 digit number");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //int thirdNum = (num1%10);
+            //int secondNum = (num1/10 % 10);
+            //int firstNum = (num1/100 % 10);
+
+
+            //Console.WriteLine(thirdNum);
+            //Console.WriteLine(secondNum);
+            //Console.WriteLine(firstNum);
+
+            //Console.ReadLine();
+        }
+    }
+ }
